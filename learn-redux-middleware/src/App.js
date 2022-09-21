d349aff640eae79ba508371be6d9dc3fee84fd09
김태민 <kw3135@gmail.com>
2022-09-21 23:53:49 +0900

    리액트를 다루는 기술_18장 정리, 실습

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import SampleContainer from './components/SampleContainer';
+import CounterContainer from './components/CounterContainer';
+
+function App() {
+  return (
+    <div>
+      {/* <SampleContainer /> */}
+      <CounterContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import rootReducer, { rootSage } from "./modules";
+import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
+);
+sagaMiddleware.run(rootSage);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+reportWebVitals();
