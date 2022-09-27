8958cc21fc18b16daf4d8c43af6fe5ac90a3efcc
김태민 <kw3135@gmail.com>
2022-09-27 13:17:28 +0900

    리액트를 다루는 기술_23장 정리, 실습

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router';
+import auth from './auth/index.js';
 import posts from './posts/index.js';
 
 const api = new Router();
-
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 // 라우터를 내보낸다.
 export default api;
--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,19 +1,19 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl.js';
+import checkLoggedIn from '../../lib/checkLoggedIn.js'
 
 const posts = new Router();
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.post('/', checkLoggedIn, postsCtrl.write);
+// posts.get('/:id', postsCtrl.read);
+// posts.delete('/:id', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+// posts.patch('/:id', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
 // 리팩토링 후 (불편하면 안써도 됨)
-// const post = new Router(); // /api/posts/:id
-// post.get('/', postsCtrl.read);
-// post.delete('/', postsCtrl.remove);
-// post.patch('/', postsCtrl.update);
-
-// posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+posts.use('/:id', postsCtrl.getPostById, post.routes());
 
 export default posts;
